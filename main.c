3a5711246c649d71b00a081738bc2508a62ba62f
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-01-27 19:21:14 +0100

    commit

---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/27 16:41:21 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/01/27 16:53:06 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include "stdio.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+
+int    main(void)
+{
+    int        fd;
+    char    *line;
+
+    fd = open("test.txt", O_RDONLY);
+    //line = get_next_line(fd);
+    //printf("%s", line);
+    while (1)
+    {
+        line = get_next_line(fd);
+        if (line == NULL)
+            break;
+        printf("%s", line);
+        if (line)
+            free(line);
+    }
+}
